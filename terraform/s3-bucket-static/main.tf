de3be8bb2e4235059b7c80975505a6458463df67
johnmelodev <joaohenrique1231500@gmail.com>
2024-08-09 15:26:11 +0100

    deploy info

---
+++ terraform/s3-bucket-static/main.tf
@@ -0,0 +1,30 @@
+provider "aws"{
+    region = "us-east-1"
+}
+
+variable "bucket_name" {
+    type = string
+}
+
+resource "aws_s3_bucket" "static_site_bucket"{
+    bucket = "static-site-${var.bucket_name}"
+
+    website {
+        index_document = "index.html"
+        error_document = "404.html"
+    }
+
+    tags = {
+        Name = "Static Site Bucket"
+        Environment = "Production"
+    }
+}
+
+resource "aws_s3_bucket_public_acess_block" "static_site_bucket"{
+    bucket = aws_s3_bucket.static_site_bucket.id
+
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
